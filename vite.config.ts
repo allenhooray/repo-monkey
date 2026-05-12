7888dbaa178b94f75889d16e91af1b955a88c757
allenhooray <allenshhu@outlook.com>
2026-05-13 03:21:07 +0800

    build: add path alias and configure baseUrl
    添加了@别名指向src目录，简化模块导入路径，同时配置tsconfig的baseUrl适配路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.config';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
